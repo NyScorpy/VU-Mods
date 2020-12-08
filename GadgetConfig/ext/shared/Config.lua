27a2ded406d1304e2dd4647fde57df5a63be3b35
NyScorpy <45854252+NyScorpy@users.noreply.github.com>
2020-12-08 21:15:05 +0100

    Add files via upload

---
+++ GadgetConfig/ext/shared/Config.lua
@@ -0,0 +1,11 @@
+Config = {
+	maxC4Count = -1,
+	ammoCountC4 = -1,
+	despawnC4OnDeath = false,
+	maxAtMineCount = -1,
+	ammoCountAtMine = -1,
+	despawnAtMineOnDeath = false,
+	maxClaymoreCount = -1,
+	ammoCountClaymore = -1,
+	despawnClaymoreOnDeath = false
+}
