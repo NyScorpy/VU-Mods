d4fe8844fc1aaca81ce0fdd789cf6612c22192f8
NyScorpy <45854252+NyScorpy@users.noreply.github.com>
2020-12-08 21:36:50 +0100

    Add files via upload

--- GadgetConfig/ext/shared/Config.lua
+++ GadgetConfig/ext/shared/Config.lua
@@ -1,11 +1,11 @@
 Config = {
-	maxC4Count = -1,
-	ammoCountC4 = -1,
-	despawnC4OnDeath = false,
-	maxAtMineCount = -1,
-	ammoCountAtMine = -1,
-	despawnAtMineOnDeath = false,
-	maxClaymoreCount = -1,
-	ammoCountClaymore = -1,
-	despawnClaymoreOnDeath = false
+	maxC4Count = -1, --maximum Amount of C4 a player can place, default = 6
+	ammoCountC4 = -1, --maximum Amount of C4 a player can carry, default = 3
+	despawnC4OnDeath = false, --despawn C4 if the player dies, default = true
+	maxAtMineCount = -1, --maximum Amount of AT-Mines a player can place, default = 6
+	ammoCountAtMine = -1, --maximum Amount of AT-Mines a player can carry, default = 3
+	despawnAtMineOnDeath = false, --despawn AT-Mines if the player dies, default = false
+	maxClaymoreCount = -1, --maximum Amount of Claymores a player can place, default = 2
+	ammoCountClaymore = -1, --maximum Amount of Claymores a player can carry, default = 1
+	despawnClaymoreOnDeath = false --despawn Claymores if the player dies, default = true
 }
