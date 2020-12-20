73d53cc0e896101ca6bd4ec76f12ca1d6967a1be
NyScorpy <45854252+NyScorpy@users.noreply.github.com>
2020-12-20 11:56:06 +0100

    Add files via upload

--- AntiAirBots/ext/shared/Config.lua
+++ AntiAirBots/ext/shared/Config.lua
@@ -1,6 +1,7 @@
 Config = {
-	fireRate = 30,
-	projectileSpeed = 200,
-	projectileTimeToLive = 20,
-	targetBotTimeToLive = 60
+	-- changing projectileSpeed or projectileTimeToLive will also change the maximum range, maxRange = projectileSpeed * projectileTimeToLive
+	fireRate = 30, --firerate of SMAW and RPG
+	projectileSpeed = 200, --projectilespeed of SMAW and RPG
+	projectileTimeToLive = 20, --time after projectiles will be destroyed if the don't hit anything
+	targetBotTimeToLive = 60 --time after the test targets will be destroyed
 }
