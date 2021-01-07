426b183fcc5d5a7d5beb3f2924c7effa62432c06
NyScorpy <45854252+NyScorpy@users.noreply.github.com>
2021-01-07 23:55:27 +0100

    Add files via upload

--- AntiAirBots/ext/shared/Config.lua
+++ AntiAirBots/ext/shared/Config.lua
@@ -2,6 +2,6 @@ Config = {
 	--changing projectileSpeed or projectileTimeToLive will also change the maximum range, maxRange = projectileSpeed * projectileTimeToLive
 	fireRate = 30, --firerate of SMAW and RPG
 	projectileSpeed = 200, --projectilespeed of SMAW and RPG
-	projectileTimeToLive = 20, --time after SMAW and RPG projectiles will be destroyed if the don't hit anything
+	projectileTimeToLive = 20, --time after SMAW and RPG projectiles will be destroyed if they don't hit anything
 	targetBotTimeToLive = 60 --time after the test targets will be destroyed
 }
--- AntiAirBots/ext/shared/__init__.lua
+++ AntiAirBots/ext/shared/__init__.lua
@@ -55,3 +55,49 @@ ResourceManager:RegisterInstanceLoadHandler(Guid('E7F8EC1A-E8F5-11DF-AC96-84E6B0
 	weaponFiringData.weaponSway = nil
 end)
 
+--[[
+--Bundle mounting
+
+Events:Subscribe('Level:LoadResources', function()
+	ResourceManager:MountSuperBundle('default_settings_win32')
+	ResourceManager:MountSuperBundle('globals')
+	ResourceManager:MountSuperBundle('ui')
+	ResourceManager:MountSuperBundle('chunks0')
+	ResourceManager:MountSuperBundle('chunks1')
+	ResourceManager:MountSuperBundle('chunks2')
+	ResourceManager:MountSuperBundle('mpchunks')
+	ResourceManager:MountSuperBundle('levels/mp_012/mp_012')
+end)
+
+Hooks:Install('ResourceManager:LoadBundles', 100, function(hook, bundles, compartment)
+	if #bundles == 1 and bundles[1] == SharedUtils:GetLevelName() then
+		print('Injecting bundles.')
+
+		bundles = {
+			'levels/mp_012/mp_012',
+			'levels/mp_012/conquest_large',
+			bundles[1]
+		}
+
+		hook:Pass(bundles, compartment)
+	end
+end)
+
+Events:Subscribe('Level:RegisterEntityResources', function(levelData)
+	local registry = RegistryContainer(ResourceManager:SearchForInstanceByGuid(Guid('320240BC-173A-5E32-CA75-51E15AC01313')))
+	ResourceManager:AddRegistry(registry, ResourceCompartment.ResourceCompartment_Game)
+end)
+]]
+
+
+
+
+
+--[[
+Events:Subscribe('BundleMounter:GetBundles', function(bundles)
+	Events:Dispatch('BundleMounter:LoadBundles', 'Levels/MP_012/MP_012', {
+	  'Levels/MP_012/MP_012',
+	  'Levels/MP_012/Conquest_Large',
+	})
+end)
+]]
