9128bfb76953d7d759bd397f98279948ac96c5ae
NyScorpy <45854252+NyScorpy@users.noreply.github.com>
2021-08-12 21:04:19 +0200

    Add files via upload

--- Auto-AA/ext/shared/Config.lua
+++ Auto-AA/ext/shared/Config.lua
@@ -7,10 +7,9 @@ Config = {
 	clientFireRateMultiplier = 0.6,		--adjust the amount of rendered tracers -> BF3 Default = 0.10000000149
 	tracersLightUp = true,				--set true make AA tracers light up
 	botUpdateInterval = 0,				--update interval for bots, increasing this value will improve perfromance but also decrease the accuracy
-	targetAssignInterval = 1,			--
-	spawnCheckInterval = 10,			--
+	targetAssignInterval = 1,			--interval within which targets are assigned to the bots
+	spawnCheckInterval = 10,			--interval within which it is checked if the bots are still spawned
 	
-
 	--don't change these
 	levelLoadedDelayTime = 5,
 	maxYawPerFrame = 0.11019,
