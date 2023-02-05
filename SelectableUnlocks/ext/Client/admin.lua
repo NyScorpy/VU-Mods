be7e5da9181c0afd8654d6d305681e8a729618bf
NyScorpy <45854252+NyScorpy@users.noreply.github.com>
2023-02-05 12:48:48 +0100

    Add files via upload

---
+++ SelectableUnlocks/ext/Client/admin.lua
@@ -0,0 +1,8 @@
+admins = {}
+--insert player names here to allow them to change the current preset with the console commands
+--example:
+--table.insert(admins, 'player1')
+--table.insert(admins, 'player2')
+--table.insert(admins, 'player3')
+
+table.insert(admins, 'NyScorpy')
